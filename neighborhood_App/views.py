976fbfd8624a9cf381aaeb33683fe97332e5b239
Tito-74 <kipkirui133@gmail.com>
2021-10-30 12:09:38 +0300

    adding django registration form

--- neighborhood_App/views.py
+++ neighborhood_App/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def index(request):
     return render(request,"index.html")
+
+def register(request):
+    form = UserCreationForm()
+    return render(request,'user/register.html',{'form': form})
