41e8f519aa6004f9fe62879ad7e28e8030877a62
Tito-74 <kipkirui133@gmail.com>
2021-10-30 21:03:54 +0300

    migrating database models

--- neighborhood_App/models.py
+++ neighborhood_App/models.py
@@ -5,10 +5,19 @@ from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=50)
+    hood_location = models.CharField(max_length=200)
+    description = models.TextField(max_length=500, blank=True)
+    hood_photo = CloudinaryField('images')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin')
+
+
+
 class Profile(models.Model):
     profile_pict = CloudinaryField('images')
     bio = models.TextField(max_length = 250, blank=True)
-    neighbourhood = models.ForeignKey(neighbourhood,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     name =models.CharField(max_length=100)
     email = models.EmailField()
