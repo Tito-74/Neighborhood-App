17c98b818a120f2fa6af3f858d6a6d7d965b9097
Tito-74 <kipkirui133@gmail.com>
2021-11-03 16:03:52 +0300

    testing profile model

--- neighborhood_App/tests.py
+++ neighborhood_App/tests.py
@@ -23,3 +23,30 @@ class neighbourhoodTestClass(TestCase):
         self.kinoo.delete_neighbourhood('kinoo')
         hood = neighbourhood.objects.all()
         self.assertTrue(len(hood)==0)
+
+
+class ProfileTestClass(TestCase):
+    #Set up method
+
+    def setUp(self):
+        
+        self.new_profile = Profile(user_id=2,hood_id=3,bio="just testing", email='kipkirui@gmail.com',name="tito",profile_pic="default.jpeg")
+
+  
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+    
+    def test_save_method(self):
+        self.new_profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+    def test_delete_method(self):
+        self.new_profile.save_profile()
+        self.new_profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)==0) 
+    
+    def tearDown(self):
+        Profile.objects.all().delete()
